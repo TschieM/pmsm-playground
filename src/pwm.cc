a5065d6096319084931b5fff8b65f495d63e5fd8
qiming yu <qiming_yu@outlook.com>
2023-05-21 09:57:44 +0200

    update pwm to add callback for interrupts

--- src/pwm.cc
+++ src/pwm.cc
@@ -17,9 +17,21 @@ void Pwm::setCompareReg(const uint32_t val){
     CCR = val;
 }
 
-void Pwm::run(void (*cb_func)(void)){
+void Pwm::setCallBackAtCCR0(void (*cb_fun)(void)) {
+    ccr0_cb = cb_fun;
+}
+
+void Pwm::setCallBackAtCCRMax(void (*cb_fun)(void)) {
+    ccrMax_cb = cb_fun;
+}
+
+void Pwm::run(void (*cb_func_high)(void), void (*cb_func_low)(void)){
     if(cnt_up) cnt_up = (cnt++ < maxCnt);
     else cnt_up = (cnt-- <= 0);
 
-    if(cnt >= CCR) cb_func();
+    if(cnt >= CCR) cb_func_high();
+    else cb_func_low();
+
+    if(cnt == maxCnt) ccrMax_cb();
+    if(cnt == 0) ccr0_cb();
 }
--- src/pwm.h
+++ src/pwm.h
@@ -9,12 +9,16 @@ class Pwm {
         ~Pwm();
         void setMaxCount(const uint32_t val);
         void setCompareReg(const uint32_t val);
-        void run(void (*cb_func)(void));
+        void setCallBackAtCCR0(void (*cb_fun)(void));
+        void setCallBackAtCCRMax(void (*cb_fun)(void));
+        void run(void (*cb_func_high)(void), void (*cb_func_low)(void));
     private:
         uint32_t maxCnt;
         uint32_t CCR;
         uint32_t cnt;
         bool cnt_up;
+        void (*ccr0_cb)(void);
+        void (*ccrMax_cb)(void);
 };
 
 #endif
